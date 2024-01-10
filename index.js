e4392077807da0218ad96687ba3fd24f7d30e1a4
Aastha Rai <aastharai2003@gmail.com>
2024-01-10 17:53:55 +0530

    uploaded logos annd some basic UI changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function openSection(TabLink, navLink) {
   let activee = document.getElementsByClassName("activee");
   let activeLink = document.getElementsByClassName("active");
-  for (let i = 0; i < 1; i++) {
-    activee[i].className = "sectionContent";
-  }
+  val = activee[0]
+  val.className = "sectionContent";
+  
   for(let i=0; i<1;i++){
     activeLink[i].className="navText"
   }
   document.getElementById(navLink).className = "active";
-  document.getElementById(TabLink).className = "activee";
+  tab=document.getElementById(TabLink)
+  tab.className = "activee";
 }
